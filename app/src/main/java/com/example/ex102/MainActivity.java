a6aa709d84fad065519d3dd1c3eb85545b144ba8
OriOfek <oriofek106@gmail.com>
2020-12-03 11:34:59 +0200

    with credits

--- app/src/main/java/com/example/ex102/MainActivity.java
+++ app/src/main/java/com/example/ex102/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -164,5 +167,46 @@ public class MainActivity extends AppCompatActivity {
 
         // Show the Alert Dialog box
         alertDialog.show();
+    }
+    /**
+     * onCreateContextMenu
+     * Short description.
+     * onCreateContextMenu listener use for the ContextMenu
+     * <p>
+     *     ContextMenu menu
+     *     View v
+     *     ContextMenu.ContextMenuInfo menuInfo
+     *
+     * @param  menu - the object,v - the item that selected ,menuInfo - the info
+     * @return	true if it success
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.generalmenu, menu);
+        return true;
+    }
+
+    /**
+     * onOptionsItemSelected
+     * Short description.
+     * what happen if an item was selected
+     * <p>
+     *     MenuItem item
+     *
+     * @param  item - the menuItem
+     * @return	true if it success
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String whatClicked = (String) item.getTitle();
+
+        if(whatClicked.equals("credits"))
+        {
+            si = new Intent(this, credits.class);
+            startActivity(si);
+        }
+
+        return  true;
     }
 }
---
+++ app/src/main/java/com/example/ex102/credits.java
@@ -0,0 +1,58 @@
+package com.example.ex102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+    /**
+     * onCreateContextMenu
+     * Short description.
+     * onCreateContextMenu listener use for the ContextMenu
+     * <p>
+     *     ContextMenu menu
+     *     View v
+     *     ContextMenu.ContextMenuInfo menuInfo
+     *
+     * @param  menu - the object,v - the item that selected ,menuInfo - the info
+     * @return	true if it success
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.generalmenu, menu);
+        return true;
+    }
+
+    /**
+     * onOptionsItemSelected
+     * Short description.
+     * what happen if an item was selected
+     * <p>
+     *     MenuItem item
+     *
+     * @param  item - the menuItem
+     * @return	true if it succed
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String whatClicked = (String) item.getTitle();
+
+        if(whatClicked.equals("Main Activity"))
+        {
+            finish();
+        }
+
+        return  true;
+    }
+}
